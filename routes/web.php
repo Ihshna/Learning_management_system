809c59d9386c6759226d2533291646a8311757a0
Ihshna <ihshanaimroos18@gmail.com>
2025-04-07 21:06:10 +0530

    Create controllers & manual routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+
+Route::get('/', fn() => redirect('/login'));
+
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Dashboards
+Route::middleware(['auth', 'role:super_admin'])->get('/super-admin/dashboard', fn() => view('dashboard.superadmin'));
+Route::middleware(['auth', 'role:admin'])->get('/admin/dashboard', fn() => view('dashboard.admin'));
+Route::middleware(['auth', 'role:student'])->get('/student/dashboard', fn() => view('dashboard.student'));
+
