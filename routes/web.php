37cf7c3aa45179d0468c214870644ccde845b465
AmeeraShara <ameerashara12@gmail.com>
2025-04-08 11:40:03 +0530

    changes in web

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\AdminController;
+
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
 
 Route::get('/', fn() => redirect('/login'));
 
-Route::get('/login', [LoginController::class,'showLogin'])->name('login');
-Route::post('/login',[LoginController::class,'login']);
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/register', [RegisterController::class,'showRegister'])->name('register');
-Route::post('/register',[RegisterController::class,'register']);
+// Dashboards (ensure only authenticated users can access)
+Route::get('superadmin/dashboard', [SuperAdminController::class, 'index'])->name('superadmin.dashboard');
+Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+Route::get('student/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
 
-Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+// Logout
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-//dashboards
-Route::middleware(['auth','role:super_admin'])->get('/super-admin/dashboard',fn() => view('dashboard.superadmin'));
-Route::middleware(['auth','role:admin'])->get('/admin/dashboard',fn() => view('dashboard.admin'));
-Route::middleware(['auth','role:student'])->get('/student/dashboard',fn() => view('dashboard.student'));
-
-Route::get('/', function () {
-    return view('welcome');
-});
