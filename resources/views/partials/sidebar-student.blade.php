8dc9ac1aa8ead1919532c3cdff78403b22787cb9
Ihshna <ihshanaimroos18@gmail.com>
2025-04-09 20:54:59 +0530

    Sidebar update

--- resources/views/partials/sidebar-student.blade.php
+++ resources/views/partials/sidebar-student.blade.php
@@ -1,12 +1,30 @@
+<div class="accordion" id="studentSidebar">
 
-    <a href="#">Dashboard</a>
-    <a href="#">My Courses</a>
-    <a href="#">Assignments</a>
-    <div class="sub-item">View Assignments</div>
-    <div class="sub-item">Submit Assignments</div>
-    <a href="#">Results</a>
-    <div class="sub-item">View Results</div>
-    <a href="#">Messages</a>
-    <div class="sub-item">Inbox</div>
-    <div class="sub-item">Send Message</div>
-    <a href="#">Profile</a>
+    <!-- Dashboard -->
+    <a href="{{ url('/student/dashboard') }}" class="d-block">Dashboard</a>
+
+    <!-- My Courses -->
+    <a href="{{ url('/student/my-courses') }}" class="d-block">My Courses</a>
+
+    <!-- Assignments -->
+    <div class="accordion-item bg-transparent border-0">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed bg-transparent text-white" type="button" data-bs-toggle="collapse" data-bs-target="#assignmentsMenu">
+                Assignments
+            </button>
+        </h2>
+        <div id="assignmentsMenu" class="accordion-collapse collapse" data-bs-parent="#studentSidebar">
+            <div class="accordion-body p-0">
+                <a href="{{ url('/student/submit-assignment') }}" class="d-block ps-4">Submit Assignments</a>
+                <a href="{{ url('/student/view-submitted') }}" class="d-block ps-4">View Submitted</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Grades -->
+    <a href="{{ url('/student/grades') }}" class="d-block">Track Grades</a>
+
+    <!-- Announcements -->
+    <a href="{{ url('/student/announcements') }}" class="d-block">View Announcements</a>
+
+</div>
